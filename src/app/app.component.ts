af2b5ba6b914968dc6affda4ff99d46ff7dc6df9
GeorgiSevdalov <nojegoje@gmail.com>
2022-07-27 16:29:24 +0300

    angular beginners exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularBeginner';
+  title = "Hello";
+  getMin(a:any, b:any) {
+    if (a < b) {
+      return a;
+    }
+    return b;
+  }
 }
